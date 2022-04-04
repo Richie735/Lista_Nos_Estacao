94ae3f110f77bbede795df4b4e3b3999a4cdaac0
Richie735 <richierichie735@gmail.com>
2022-04-04 23:48:55 +0100

    Update

--- ex01/ex01.cpp
+++ ex01/ex01.cpp
@@ -9,17 +9,16 @@
 
 using namespace std;
 
+typedef enum _BOOLEAN { FALSE = 0, TRUE = 1 } BOOLEAN;
+typedef enum _LIST_LOCATION { LIST_START, LIST_END } LIST_LOCATION;
+typedef enum _STATUS { OK, ERROR } STATUS;
+
 #define DATA(node) ((node)->data)
 #define NEXT(node) ((node)->next)
 
 #define EMPTY NULL
 #define NO_LINK NULL
-#define MAX 20
-
-
-typedef enum _BOOLEAN { FALSE = 0, TRUE = 1 } BOOLEAN;
-typedef enum _LIST_LOCATION { LIST_START, LIST_END } LIST_LOCATION;
-typedef enum _STATUS { OK, ERROR } STATUS;
+#define MAX 50
 
 typedef struct _LIST_NODE
 {
@@ -33,13 +32,11 @@ typedef struct _ESTACAO
 {
 	char desig[MAX];
 	float custo;
-	BOOLEAN ativa,no;
+	BOOLEAN ativa, no;
 }ESTACAO;
 
-
-
 void initList(LIST* list);
-void FreeList(LIST* list);		
+void FreeList(LIST* list);
 BOOLEAN emptyList(LIST list);
 LIST_NODE* NewNode(void* data);
 STATUS RemoveNode(LIST* list, void* data);
@@ -47,57 +44,108 @@ STATUS InsertIni(LIST* list, void* data);
 STATUS InsertEnd(LIST* list, void* data);
 
 int ListSize(LIST list);
+LIST ChooseList(LIST line1, LIST line2, LIST line3, LIST line4, LIST line5);
 void ShowValues(LIST list);
 void ShowStatus(LIST list);
 
-void* FindStationName(LIST list, char* str);
+void AddEstacao(LIST* list);
+void RemoveEstacao(LIST* list);
+ESTACAO* FindStationName(LIST list, char* str);
 
 int Menu();
 
-STATUS SaveList(LIST list, const char* filename);
 STATUS ReadLine(LIST* list, const char* filename);
+STATUS SaveLine(LIST list, const char* filename);
 
-/****************************************************************
-* Programa principal
-****************************************************************/
+
 int main()
 {
-	LIST line1 = NULL, line2 = NULL, line3 = NULL, line4 = NULL, line5 = NULL;
+	LIST linha1 = NULL, linha2 = NULL, linha3 = NULL, linha4 = NULL, linha5 = NULL, linha = NULL;
 	ESTACAO* ptr = NULL;
-	if (ReadLine(&line1, "LinhaTeste.txt") == OK){  //&&  ReadLine(&line2, "Linha2.txt") == OK  &&  ReadLine(&line3, "Linha3.txt") == OK  &&  
-		//ReadLine(&line4, "Linha4.txt") == OK  &&  ReadLine(&line5, "Linha5.txt") == OK) {
+
+	char str[20];
+	void* pt = NULL;
+
+	if ((ReadLine(&linha1, "Linha1.txt") == OK)
+		&& (ReadLine(&linha2, "Linha2.txt") == OK)
+		&& (ReadLine(&linha3, "Linha3.txt") == OK)
+		&& (ReadLine(&linha4, "Linha4.txt") == OK)
+		&& (ReadLine(&linha5, "Linha5.txt") == OK))
+	{
 		do {
 			switch (Menu()) {
-			case 1:
-				ShowStatus(line1);
-				break;
+			case 1: // Ativar/Desativar uma estacao
+			{
+				printf("\n\n-----------------------------------\n");
+				printf("\tAtivar / Desativar\t\n");
+				printf("-----------------------------------\n");
 
-			case 2:
-				break;
+				linha = ChooseList(linha1, linha2, linha3, linha4, linha5);
 
-			case 3:
-				break;
+				printf(" >Insere o nome da estacao :  ");
+				while ((getchar()) != '\n');
+				gets_s(str);
 
-			case 4:
-				break;
+				if ((ptr = FindStationName(linha, str)) != NULL)
+				{
 
-			case 5:
+					if ((ptr->ativa) == FALSE)
+					{
+						ptr->ativa = TRUE;
+						printf(" -Estacao ativada\n");
+					}
+					else
+					{
+						ptr->ativa = FALSE;
+						printf(" -Estacao desativada\n");
+					}
+				}
+				printf("\n\n");
 				break;
+			}
 
-			case 0:
+			case 2: // Adicionar uma estacao
+			{
+				linha = ChooseList(linha1, linha2, linha3, linha4, linha5);
+				AddEstacao(&linha);
+				ShowValues(linha);
+				break;
+			}
+
+			case 3: // Eliminar uma estacao
+			{
+				linha = ChooseList(linha1, linha2, linha3, linha4, linha5);
+				RemoveEstacao(&linha);
+				break;
+			}
+
+			case 4: // Guardar as linhas da rede
+			{
+				break;
+			}
+
+			case 5: // Calcular custo de viagem
+			{
+				break;
+			}
+
+			case 0: // Sair
+			{
 				return 0;
 				break;
+			}
 
 			default:
-				printf("\n   <Opcao Invalida>   \n\n");
+			{
+				printf("Opção inválida");
 				break;
+			}
 			}
 		} while (true);
 	}
-	else  printf("     <<ERRO>>\n A ler as linhas\n\n");
+	else printf("<<ERROR - leitura de linhas>>\n");
 
-
-	return (0);
+	return 0;
 }
 
 
@@ -109,7 +157,7 @@ int main()
 /****************************************************************
 * Funcao: Inicializa a lista
 *
-* Parametros:	list - apontador para lista (duplo apontador para o primeiro no')
+* Parametros: list - apontador para lista (duplo apontador para o primeiro no')
 * 
 * Saida: void
 ***************************************************************/
@@ -143,7 +191,7 @@ void FreeList(LIST* list)
 /****************************************************************
 * Funcao: verifica se a lista é vazia
 *
-* Parametros:	list - apontador para lista
+* Parametros: list - apontador para lista
 * 
 * Saida: TRUE se a lista for vazia, ERROR caso contrário
 ***************************************************************/
@@ -156,7 +204,7 @@ BOOLEAN emptyList(LIST list)
 /****************************************************************
 * Funcao: Cria um no'
 *
-* Parametros:	data - apontador generico para os dados a inserir no no' criado
+* Parametros: data - apontador generico para os dados a inserir no no' criado
 * 
 * Saida: apontador para o no' criado ou NULL em caso de erro
 ***************************************************************/
@@ -177,10 +225,10 @@ LIST_NODE* NewNode(void* data)
 *
 * Parametros:   list - apontador para lista
 *				data - apontador generico para os dados a inserir no no' criado
-* 
+*
 * Saida: OK se o nó foi inserido na LISTA e ERROR caso contrário
 ***************************************************************/
-STATUS RemoveNode(LIST * list, void* data)
+STATUS RemoveNode(LIST* list, void* data)
 {
 	LIST_NODE* temp = *list, * ant = *list;
 
@@ -202,14 +250,13 @@ STATUS RemoveNode(LIST * list, void* data)
 	return ERROR;
 }
 
-
 /****************************************************************
 * Funcao: Insere um no' no inicio da lista
 *
-* Parametros:	list - apontador para lista (duplo apontador para o primeiro no')
-*				data - apontador generico para os dados a inserir no no' criado
-*
-* Saida: OK se o nó foi inserido na LISTA e ERROR caso contrário
+* Parametros: list - apontador para lista (duplo apontador para o primeiro no')
+*			  data - apontador generico para os dados a inserir no no' criado
+* 
+Saida: OK se o nó foi inserido na LISTA e ERROR caso contrário
 ***************************************************************/
 STATUS InsertIni(LIST* list, void* data)
 {
@@ -227,8 +274,8 @@ STATUS InsertIni(LIST* list, void* data)
 /****************************************************************
 * Funcao: Insere um no' no fim da lista
 *
-* Parametros:	list - apontador para lista (duplo apontador para o primeiro no')
-*				data - apontador generico para os dados a inserir no no' criado
+* Parametros: list - apontador para lista (duplo apontador para o primeiro no')
+*			  data - apontador generico para os dados a inserir no no' criado
 * 
 * Saida: OK se o nó foi inserido na LISTA e ERROR caso contrário
 ***************************************************************/
@@ -255,7 +302,7 @@ STATUS InsertEnd(LIST* list, void* data)
 /***************************************************************************
 * Funcao: calcula quantos elementos contem a lista
 *
-* Parametros:	list - apontador para uma lista
+* Parametros: list - apontador para uma lista
 * 
 * Saida: numero de elementos da lista
 ***************************************************************************/
@@ -271,10 +318,51 @@ int ListSize(LIST list)
 }
 
 
+/***************************************************************************
+* Funcao: calcula quantos elementos contem a lista
+*
+* Parametros: line+ - apontador para uma lista
+* 
+* Saida: numero de elementos da lista
+***************************************************************************/
+LIST ChooseList(LIST line1, LIST line2, LIST line3, LIST line4, LIST line5)
+{
+	int op;
+
+	printf(" >Numero da linha : ");
+	scanf("%d", &op);
+
+	switch (op) {
+		case 1: 
+			return line1;
+			break;
+		
+		case 2:
+			return line2;
+			break;
+
+		case 3: 
+			return line3;
+			break;
+
+		case 4:
+			return line4;
+			break;
+		
+		case 5:
+			return line5;
+			break;
+		
+		default:
+			printf("Linha nao existe \n");
+			break;
+	}
+}
+
 /****************************************************************
 * Funcao: Escreve no ecra o conteudo da lista
 *
-* Parametros:	list - apontador para o primeiro no'
+* Parametros: list - apontador para o primeiro no'
 * 
 * Saida: void
 ***************************************************************/
@@ -285,16 +373,13 @@ void ShowValues(LIST list)
 		printf("\nLista vazia.\n");
 		return;
 	}
-
-	printf("LISTA ->");
-	
+	printf("INICIO LISTA:\n");
 	while (list != NULL)
 	{
-		printf(" %s;%.2f;%d;%d ->", ((ESTACAO*)DATA(list))->desig, ((ESTACAO*)DATA(list))->custo, ((ESTACAO*)DATA(list))->ativa, ((ESTACAO*)DATA(list))->no);
+		printf(" %s;%.1f;%d;%d;\n", ((ESTACAO*)DATA(list))->desig, ((ESTACAO*)DATA(list))->custo, ((ESTACAO*)DATA(list))->ativa, ((ESTACAO*)DATA(list))->no);
 		list = NEXT(list);
 	}
-	
-	printf(" FIM");
+	printf("--FIM--\n");
 }
 
 
@@ -302,16 +387,71 @@ void ShowValues(LIST list)
 * Funcao: Mostra o tamanho e o conteudo da lista
 *
 * Parametros:	list - apontador para o primeiro no'
-* 
+*
 * Saida: void
 ***************************************************************/
 void ShowStatus(LIST list)
 {
 	printf("\n");
-	printf("\nTamanho = %d\n", ListSize(list));
-	printf("ESTACOES:\n");
+	printf("\n---Tamanho = %d---\n", ListSize(list));
 	ShowValues(list);
 	printf("\n");
+
+}
+
+
+/***************************************************************************
+* Funcao: Insere uma estacao no fim de uma lista
+*
+* Parametros: list - apontador para o primeiro no'
+* 
+* Saida: void
+***************************************************************************/
+void AddEstacao(LIST* list)
+{
+	ESTACAO* ptr;
+	int ativa, no;
+
+	if (((ptr = (ESTACAO*)malloc(sizeof(ESTACAO))) != NULL) && (InsertEnd(list, ptr) == OK))
+	{
+		printf(" Designação: ");
+		scanf("%s", &ptr->desig);
+
+		printf(" Custo: ");
+		scanf("%f", &ptr->custo);
+		
+		printf(" Ativo (0/1): ");
+		scanf("%d", &ativa);
+		if (ativa) ptr->ativa = TRUE;
+		else ptr->ativa = FALSE;
+
+		printf(" No (0/1): ");
+		scanf("%d", &no);
+		if (no) ptr->no = TRUE;
+		else ptr->no = FALSE;
+	}
+}
+
+
+/***************************************************************************
+* Funcao: Remove uma estacao de uma lista
+*
+* Parametros: list - apontador para lista (duplo apontador para o primeiro no')
+*
+* Saida: void
+***************************************************************************/
+void RemoveEstacao(LIST* list)
+{
+	char str[30];
+	ESTACAO* ptr;
+
+	printf("\n\n\n");
+	printf("Designação da estação: ");
+	while ((getchar()) != '\n');
+	gets_s(str);
+
+	if ((ptr = FindStationName(*list, str)) != NULL) RemoveNode(list, ptr);
+	else printf("Estação inexistente\n");
 
 }
 
@@ -324,15 +464,19 @@ void ShowStatus(LIST list)
 *
 * Saida: no desejado
 ***************************************************************/
-void* FindStationName(LIST list, char* str)
+ESTACAO* FindStationName(LIST list, char* str)
 {
 	while (list != NULL)
 	{
-		if (!strcmp(((ESTACAO*)list->data)->desig, str))	return list->data;
-		
-		list = list->next;
+		if (!strcmp(((ESTACAO*)list->data)->desig, str))
+		{
+			printf(" -Estacao encontrada\n");
+			return (ESTACAO*)DATA(list);
+		}
+		list = NEXT(list);
 	}
 	return NULL;
+
 }
 
 
@@ -348,7 +492,7 @@ int Menu()
 	int op;
 	do {
 		printf("\n===================================\n");
-		printf("\tMENU\t\n");
+		printf("\t\tMENU\t\n");
 		printf("===================================\n");
 		printf("  1. Ativar/Desativar uma estacao\n");
 		printf("  2. Adicionar uma estacao\n");
@@ -360,7 +504,7 @@ int Menu()
 		printf("\n >> ");
 		scanf_s("%d", &op);
 	} while (op < 0 && op >5);
-	
+
 	return op;
 }
 
@@ -370,28 +514,34 @@ int Menu()
 *
 * Parametros:	list - apontador para lista (duplo apontador para o primeiro no')
 *				filename - nome do ficheiro onde guardar
-* 
+*
 * Saida: OK se a lista for guardada com sucesso e ERROR caso o ficheiro nao seja aberto
 ***************************************************************/
-STATUS SaveList(LIST list, const char* filename)
+STATUS SaveLine(LIST list, const char* filename)
 {
-	ofstream file;
-	file.open(filename);
+	FILE* fp = NULL;
+	ESTACAO* pt = NULL;
+	int ativa = 0, no = 0;
 
-	if (file.is_open()) {
+	if ((fp = fopen(filename, "w")) != NULL)
+	{
 		while (list != NULL)
 		{
-			file << ((ESTACAO*)DATA(list))->desig << ';' << ((ESTACAO*)DATA(list))->custo << ';' << ((ESTACAO*)DATA(list))->ativa << ';' << ((ESTACAO*)DATA(list))->no << ';' << endl;
+			pt = (ESTACAO*)DATA(list);
+			if (pt->ativa) ativa = 1;
+			if (pt->no) no = 1;
+			fprintf(fp, "%c;%.1f;%d;%d;\n", pt->desig, pt->custo, ativa, no);
 			list = NEXT(list);
 		}
+		fclose(fp);
+		printf("Linha salva com sucesso\n");
+		return OK;
 	}
 	else {
-		printf("Erro a Abrir %s", filename);
-		return(ERROR);
+		fclose(fp);
+		printf("ERRO\n");
+		return ERROR;
 	}
-
-	file.close();
-	return(OK);
 }
 
 
@@ -405,62 +555,32 @@ STATUS SaveList(LIST list, const char* filename)
 ***************************************************************/
 STATUS ReadLine(LIST* list, const char* filename)
 {
-	/*
-	ifstream file(filename);
-	//file.open(filename);
-
-	ESTACAO* pt = NULL;
-	int ativa, no;
-
-	if (file.is_open()) {
-		while (!file.eof()) {
-			if ((pt = (ESTACAO*)malloc(sizeof(ESTACAO))) != NULL && (InsertIni(list, pt)) == OK) {
-				cout << &pt->desig << ';' << &(pt->custo) << ';' << &ativa << ';' << &no << ';';
-
-				if (ativa)	pt->ativa = TRUE;
-				else pt->ativa = FALSE;
-
-				if (no)		pt->no = TRUE;
-				else pt->no = FALSE;
-			}
-			else
-			{
-				FreeList(list);
-				return ERROR;
-			}
-		}
-	}
-	
-	file.close();
-	return(OK);
-	*/
-
-	ESTACAO* pt = NULL;
 	FILE* fp = NULL;
-	int ativa, no;
-	
-	if ((fp = fopen(filename, "r")) != NULL)
+	ESTACAO* pt = NULL;
+	int ativa = 0, no = 0;
+
+	if (((fp = fopen(filename, "r")) != NULL))
 	{
 		while (!feof(fp))
 		{
-			if ((pt = (ESTACAO*)malloc(sizeof(ESTACAO))) != NULL && (InsertIni(list, pt)) == OK)
+			if (((pt = (ESTACAO*)malloc(sizeof(ESTACAO))) != NULL) && (InsertEnd(list, pt) == OK))
 			{
-				fscanf(fp, "%[^;];%f;%d;%d\n", &pt->desig, &(pt->custo), &ativa, &no);
-
-				if (ativa)	pt->ativa = TRUE;
-				else pt->ativa = FALSE;
-
-				if (no)		pt->no = TRUE;
-				else pt->no = FALSE;
+				fscanf(fp, "%[^;];%f;%d;%d;\n", pt->desig, &(pt->custo), &ativa, &no);
+				pt->ativa = pt->no = FALSE;
+				if (ativa)
+					pt->ativa = TRUE;
+				if (no)
+					pt->no = TRUE;
+				//InsertIni(list, pt);
 			}
 			else
 			{
 				FreeList(list);
-				return ERROR;
+				return (ERROR);
 			}
 		}
 		fclose(fp);
-		return OK;
+		return(OK);
 	}
-	return ERROR;
+	return(ERROR);
 }
